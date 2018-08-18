83ead1da08dbd55bc1c5594885b9f936b1aaa04d
liulilin <327544194@qq.com>
2018-08-18 18:03:19 +0800

    v2.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*^gi*#@h1nupsg6_*2e4u#d&e%5r*c5i6h0)fcp*4-o#z*lcng'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.liulilin.net' , '127.0.0.1' , 'localhost']
 
 
 # Application definition
